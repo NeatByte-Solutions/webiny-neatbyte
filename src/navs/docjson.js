2f3f77b6ea412d4728939fc948ad82597cb0c588
Andriy <andriy.muhayliv@neatbyte.solutions>
2022-01-21 11:25:40 +0200

    Json example

---
+++ src/navs/docjson.js
@@ -0,0 +1,36 @@
+export const documentationNav2 = [
+  {
+    type: 'expandable',
+    title: 'Create Custom Application',
+    links: [
+      {
+        type: 'section',
+        title: 'First section',
+      },
+      {
+        type: 'expandable',
+        title: 'Project Organization',
+        links: [
+          { type: 'link', link: 'https://', title: 'Project Application' },
+          { type: 'link', link: 'https://', title: 'Applications and Packages' },
+          { type: 'link', link: 'https://', title: 'Monorepo Organization' },
+        ],
+      },
+      {
+        type: 'link',
+        title: 'Project Application',
+        link: 'https://',
+      },
+      {
+        type: 'link',
+        title: 'Applications and Packages',
+        link: 'https://',
+      },
+      {
+        type: 'link',
+        title: 'Monorepo Organization',
+        link: 'https://',
+      },
+    ],
+  },
+]
