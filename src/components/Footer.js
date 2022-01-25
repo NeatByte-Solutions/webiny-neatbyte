c0604878ed4b43b242db4e91330d36455f39ed59
Uliana Sadykova <uliana.sadykova@neatbyte.solutions>
2022-01-25 13:42:42 +0200

    add font changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,13 @@
 import Link from 'next/link'
-import clsx from 'clsx'
 
 export function Footer({ previous, next }) {
   return (
-    <footer className="text-sm leading-6 my-[1.875rem] md:my-[3.75rem]">
+    <footer className="text-sm leading-6 my-[1.875rem] md:my-[3.75rem] text-dark-blue">
       {(previous || next) && (
-        <div className="text-slate-700 font-semibold grid grid-cols-2 content-center dark:text-slate-200 w-full border-t border-b border-neutral-200 pt-2 md:pt-[1.4375rem] pb-2 md:pb-[1.8125rem]">
+        <div className="grid grid-cols-2 content-center w-full border-t border-b border-neutral-200 pt-2 md:pt-[1.4375rem] pb-2 md:pb-[1.8125rem] text-sm md:text-base font-semibold">
           {previous ? (
             <Link href={previous.href}>
-              <a
-                className={`group flex items-center justify-start md:justify-end hover:text-slate-900 dark:hover:text-white pr-[2.1875rem] border-r border-neutral-200`}
-              >
+              <a className="group flex items-center justify-start md:justify-end pr-[2.1875rem] border-r border-neutral-200">
                 <img
                   src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxOCAxMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggb3BhY2l0eT0iMC41IiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGNsaXAtcnVsZT0iZXZlbm9kZCIgZD0iTTYuNjQyMSAwLjI0MzYwN0M2LjMxNDgxIC0wLjA4MTE5OTkgNS43ODM5NCAtMC4wODExOTk5IDUuNDU2NjUgMC4yNDM1ODlMMC4yNDU2MTcgNS40MTIxMkMwLjA4MTg3MDggNS41NzQyOSAtMi41Mjk3NGUtMDcgNS43ODczOCAtMi42MjI2OWUtMDcgNkMtMi43MTU2NGUtMDcgNi4yMTI2NSAwLjA4MTg3MDggNi40MjU3IDAuMjQ1NjE3IDYuNTg4MDlMNS40NTY2NSAxMS43NTY2QzUuNzgzMDUgMTIuMDgwMSA2LjMxMTk0IDEyLjA4MTQgNi42Mzk2NCAxMS43NTg4QzYuOTY3NzQgMTEuNDM2MiA2Ljk3MDgxIDEwLjkxNzMgNi42NDcwNyAxMC41OTEyTDIuOTI0NjggNi44NDI1NEwxNy4xNjE3IDYuODQyNTVDMTcuNjI0OCA2Ljg0MjU1IDE4IDYuNDU5OTkgMTggNi4wMDA5NEMxOCA1LjU0MTcgMTcuNjI0OCA1LjE1OTMyIDE3LjE2MTcgNS4xNTkzMkwyLjg2MjI2IDUuMTU5MzFMNi42NDIxIDEuNDE0NzVDNi45Njk1MiAxLjA4OTk3IDYuOTY5NTIgMC41NjgzODYgNi42NDIxIDAuMjQzNjA3WiIgZmlsbD0iIzk0QTNCOCIvPgo8L3N2Zz4K"
                   className="mr-3"
@@ -24,7 +21,7 @@ export function Footer({ previous, next }) {
           )}
           {next ? (
             <Link href={next.href}>
-              <a className="group flex items-center justify-end md:justify-start hover:text-slate-900 dark:hover:text-white pl-[2.1875rem] border-l border-neutral-200">
+              <a className="group flex items-center justify-end md:justify-start pl-[2.1875rem] border-l border-neutral-200">
                 {next.shortTitle || next.title}
 
                 <img
@@ -39,43 +36,54 @@ export function Footer({ previous, next }) {
           )}
         </div>
       )}
-      <div className="pt-[1.875rem] grid md:grid-cols-2 text-slate-500 gap-y-[1.875rem] md:gap-y-0 md:gap-x-10">
-        <div className="grid gap-y-1.5">
+      <div className="pt-[1.875rem] grid md:grid-cols-2 gap-y-[1.875rem] md:gap-y-0 md:gap-x-10">
+        <div className="grid gap-y-1.5 text-[0.875rem] md:text-[1rem] leading-7">
           <div className="flex">
             <img
               src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNiIgaGVpZ2h0PSIxMSIgdmlld0JveD0iMCAwIDYgMTEiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xIDEwLjM3NEw1LjM2ODcxIDUuNTc3NzhMMS4wMzI4MSAwLjYyNTc1NCIgc3Ryb2tlPSIjMzM0MTU1IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg=="
-              className="mr-2.5 h-6 w-1.5"
+              className="mr-2.5 h-7 w-1.5"
               alt="arrow"
             />
             <p>
-              Questions? <a href="#">Find us on slack.</a>
+              Questions? &nbsp;
+              <a href="#" className="text-orange underline hover:decoration-2">
+                Find us on slack.
+              </a>
             </p>
           </div>
           <div className="flex">
             <img
               src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNiIgaGVpZ2h0PSIxMSIgdmlld0JveD0iMCAwIDYgMTEiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xIDEwLjM3NEw1LjM2ODcxIDUuNTc3NzhMMS4wMzI4MSAwLjYyNTc1NCIgc3Ryb2tlPSIjMzM0MTU1IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg=="
-              className="mr-2.5 h-6 w-1.5"
+              className="mr-2.5 h-7 w-1.5"
               alt="arrow"
             />
             <p>
-              Found a bug on the page, <a href="#">submit an issue or a PR.</a>
+              Found a bug on the page, &nbsp;
+              <a href="#" className="text-orange underline hover:decoration-2">
+                submit an issue or a PR.
+              </a>
             </p>
           </div>
           <div className="flex">
             <img
               src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNiIgaGVpZ2h0PSIxMSIgdmlld0JveD0iMCAwIDYgMTEiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xIDEwLjM3NEw1LjM2ODcxIDUuNTc3NzhMMS4wMzI4MSAwLjYyNTc1NCIgc3Ryb2tlPSIjMzM0MTU1IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg=="
-              className="mr-2.5 h-6 w-1.5"
+              className="mr-2.5 h-7 w-1.5"
               alt="arrow"
             />
             <p>
-              Check out our <a href="#">roadmap.</a>
+              Check out our &nbsp;
+              <a href="#" className="text-orange underline hover:decoration-2">
+                roadmap.
+              </a>
             </p>
           </div>
         </div>
         <div>
-          <p>Join our developer newsletter</p>
+          <p className="text-[0.875rem] md:text-[1rem] leading-7 font-bold">
+            Join our developer newsletter
+          </p>
           <div className="bg-black mt-2.5 mb-[0.3125rem] h-10 w-full" />
-          <p>You can unsuscribe at any time.</p>
+          <p className="text-[0.75rem] leading-[0.875rem]">You can unsuscribe at any time.</p>
         </div>
       </div>
     </footer>
