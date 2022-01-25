be994e404399be34d7e32e064c40372d1f2ebaa9
tpysko <taras.pysko@neatbyte.solutions>
2022-01-25 13:06:30 +0200

    Fixed css

--- src/components/TabsNew.js
+++ src/components/TabsNew.js
@@ -1,14 +1,17 @@
 import { Tab, TabPanel, Tabs, TabList } from 'react-tabs'
+import clsx from 'clsx'
+
+import styles from './TabsNew.module.css'
 
 export function TabsNew({ children, labels }) {
   return (
-    <Tabs className="">
-      <TabList className="flex">
+    <Tabs>
+      <TabList className={clsx('flex', styles['prose'])}>
         {labels.map((obj) => {
           return <Tab>{obj.label}</Tab>
         })}
       </TabList>
-      <div>
+      <div className="bg-smoke rounded-xl p-8">
         {children.map((obj) => {
           return <TabPanel>{obj.props.children}</TabPanel>
         })}
