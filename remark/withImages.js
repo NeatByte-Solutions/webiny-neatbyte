471053fd0fbc335f0dea248d33b3ecb2cc15d5eb
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-01-26 19:06:25 +0200

    Fix case for full image path

--- remark/withImages.js
+++ remark/withImages.js
@@ -4,7 +4,7 @@ function getImageFileName(url) {
   const pathParts = url.split('/')
   const fileName = pathParts[pathParts.length - 1]
 
-  return fileName.split(".")[0]
+  return fileName.split('.')[0]
 }
 
 module.exports.withImages = () => {
@@ -13,11 +13,13 @@ module.exports.withImages = () => {
     let counter = 1
 
     function processImages(node) {
-      if (node.type === 'image' && node.url) {
+      if (node.type === 'image' && !node.url.startsWith('http')) {
         const imageImportName = addImportImage(preTree, node.url, 'Image' + counter++)
 
         node.type = 'jsx'
-        node.value = `<img src={${imageImportName}} alt="${node.alt || getImageFileName(node.url)}"></img>`
+        node.value = `<img src={${imageImportName}} alt="${
+          node.alt || getImageFileName(node.url)
+        }"></img>`
       } else if (node.children) {
         node.children.map((nodeChild) => processImages(nodeChild))
       }
