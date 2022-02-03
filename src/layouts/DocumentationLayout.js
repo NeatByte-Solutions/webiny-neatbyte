6fa2d0a67f6b4e5552130655fbd828f46aff91f6
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2022-02-03 10:39:09 +0200

    Delete unused files 12

--- src/layouts/DocumentationLayout.js
+++ src/layouts/DocumentationLayout.js
@@ -1,7 +1,6 @@
 import { SidebarLayout } from '@/layouts/SidebarLayout'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
-import socialSquare from '@/img/social-square.jpg'
+
 import { Title } from '@/components/Title'
 import { documentationNav } from '@/navs/documentation'
 
@@ -13,14 +12,6 @@ export function DocumentationLayout(props) {
       <Title suffix={router.pathname === '/' ? undefined : 'Tailwind CSS'}>
         {props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}
       </Title>
-      <Head>
-        <meta key="twitter:card" name="twitter:card" content="summary" />
-        <meta
-          key="twitter:image"
-          name="twitter:image"
-          content={`https://tailwindcss.com${socialSquare}`}
-        />
-      </Head>
       <SidebarLayout nav={documentationNav} {...props} />
     </>
   )
