78eb94ca58c6494aeac8ced9a54d42a15fa2dbbe
Oleksandr Kalyta <91054644+neatbyte-okalyta@users.noreply.github.com>
2022-02-14 18:07:46 +0200

    Removed the border on search blur background (#161)
    Co-authored-by: Aleksandr Kalyta <aleksandrkalyta@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -314,7 +314,7 @@ export function Header({ navIsOpen, onNavToggle }) {
         open={isOpen}
         onClose={() => setIsOpen}
       >
-        <Dialog.Overlay className="fixed top-[3.975rem] inset-1 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
+        <Dialog.Overlay className="fixed top-[3.975rem] inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
         <div
           className={clsx(
             'fixed z-40 top-16 hidden lg:flex items-center w-full h-[4.375rem] pl-[2.375rem] pr-[1.875rem] bg-white',
